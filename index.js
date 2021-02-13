2eb83a7938d162b45543c92db53e6c21c1de332d
G-nm <gmang2236@gmail.com>
2021-02-13 18:09:04 +0300

    pool added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 const pgSession = require("connect-pg-simple")(session);
-const pool = require("./services/dbconnection").pool;
+const { Pool } = require("pg");
 const port = process.env.PORT || 3636;
 const cors = require("cors");
 if (process.env.NODE_ENV == "dev") {
@@ -10,10 +10,19 @@ if (process.env.NODE_ENV == "dev") {
   app.use(morgan("dev"));
 }
 
+const pool = new Pool({
+  user: `${process.env.user}`,
+  host: `${process.env.host}`,
+  database: `${process.env.database}`,
+  password: `${process.env.password}`,
+  port: process.env.port,
+  ssl: process.env.NODE_ENV == "production" ? true : false,
+});
+
 app.use(
   session({
     store: new pgSession({
-      pool,
+      pool: pool,
     }),
     name: "userscookie",
     secret: "whoami",
@@ -22,9 +31,8 @@ app.use(
       maxAge: 1000 * 60 * 60 * 24,
       httpOnly: true,
       sameSite: "lax",
-      // secure: true,
     },
-    resave: true,
+    resave: false,
   })
 );
 app.use(
